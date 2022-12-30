04e55ad8ddd2d21feeb9e9185e1980a4c1fa8dd5
Ahmedmma72 <ahmedmma561@gmail.com>
2022-12-30 20:37:37 +0200

    getMatchById is added

--- src/api/controllers/guest.controller.ts
+++ src/api/controllers/guest.controller.ts
@@ -23,3 +23,26 @@ export const getMatches = async (req: Request, res: Response) => {
     res.status(401).json({ error: typedError?.message });
   }
 };
+
+export const getMatch = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    if (isNaN(id)) {
+      res.status(400).json({ error: 'Invalid match id.' });
+      return;
+    }
+    const match = await Match.getMatchById(id);
+    if (!match) {
+      res.status(404).json({ error: 'Match not found.' });
+      return;
+    }
+    // remove the timezone offset
+    match.date = new Date(
+      match.date.getTime() - match.date.getTimezoneOffset() * 60 * 1000
+    );
+    res.json({ match: match });
+  } catch (err: unknown) {
+    const typedError = err as Error;
+    res.status(401).json({ error: typedError?.message });
+  }
+};
--- src/api/routes/guest.route.ts
+++ src/api/routes/guest.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getMatches } from "../controllers/guest.controller";
+import { getMatches, getMatch } from "../controllers/guest.controller";
 
 const guestRouter = Router();
 
 guestRouter.get("/matches", getMatches);
+guestRouter.get("/matches/:id", getMatch);
 
 export default guestRouter;
